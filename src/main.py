40a8c2a9fefe233a062691a1b0b5709eb56e35b9
pavva91 <valerio.mattioli@proton.me>
2026-01-18 00:20:22 +0100

    clean

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-from fastapi_pagination import add_pagination
-from fastapi_pagination import add_pagination
 from starlette.middleware.gzip import GZipMiddleware
-from fastapi import FastAPI
+
 from api import api_router
 
 app = FastAPI(
@@ -11,9 +9,7 @@ app = FastAPI(
     root_path="/api/v1",
 )
 
-add_pagination(app)
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 # NOTE: we add here all the API routes in the api_router
 app.include_router(api_router)
-
-app.add_middleware(GZipMiddleware, minimum_size=1000)
